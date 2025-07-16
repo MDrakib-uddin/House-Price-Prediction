5cc105095d5cfccf5121b4469a00aea93a13d4a4
Rakib Uddin <rakibuddinraki2003@gmail.com>
2025-07-16 18:00:45 +0600

    Update Flask app to use 0.0.0.0 host and dynamic port for Railway deployment

--- server/server.py
+++ server/server.py
@@ -31,4 +31,6 @@ def predict_home_price():
 
 if __name__ == "__main__":
     util.load_saved_artifacts()
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
